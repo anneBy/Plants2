7e34d30dd7607301f5b56a2903b2c0873dd98b8f
MiaS <maria.schildbeck@gmx.at>
2018-07-09 14:45:42 +0200

    initial commit

---
+++ Plants2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Plants2.MvcApplication" Language="C#" %>
---
+++ Plants2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Plants2.Startup))]
+namespace Plants2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

803316c2db2a3773d04986150c64f1bd1c4b225c
MiaS <maria.schildbeck@gmx.at>
2018-07-09 18:33:28 +0200

    added-plant-mvc

---
+++ Plants2/Controllers/PlantsController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Plants2.Models;
+
+namespace Plants2.Controllers
+{
+    public class PlantsController : Controller
+    {
+        private Plant2DBContext db = new Plant2DBContext();
+
+        // GET: Plants
+        public ActionResult Index(string hLevelName, string searchString)
+        {
+            var LevelLst = new List<string>();
+
+            var LevelQry = from d in db.Plants
+                           orderby d.HLevelName
+                           select d.HLevelName;
+
+            LevelLst.AddRange(LevelQry.Distinct());
+            ViewBag.hLevelName = new SelectList(LevelLst);
+
+            var plants = from p in db.Plants
+                         select p;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                plants = plants.Where(s => s.Name.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(hLevelName))
+            {
+                plants = plants.Where(x => x.HLevelName == hLevelName);
+            }
+
+            plants = plants.OrderBy(p => p.Name);
+
+            return View(plants);
+        }
+
+        // GET: Plants/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Plant plant = db.Plants.Find(id);
+            if (plant == null)
+            {
+                return HttpNotFound();
+            }
+
+            //Plant parent = plant.Parent;
+            //string parName = parent.Name;
+
+            //ViewBag.parentName = parName;
+            return View(plant);
+        }
+
+        // GET: Plants/Create
+        public ActionResult Create()
+        {
+            var ParentLst = new List<string>();
+
+            var ParentQry = from p in db.Plants
+                            orderby p.Name
+                            select p.Name;
+
+            ParentLst.AddRange(ParentQry);
+            ViewBag.parentNameList = new SelectList(ParentLst);
+
+            var plant = new Plant();
+
+            return View(plant);
+        }
+
+        // POST: Plants/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PlantID,Name,ParentID,HLevelName")] Plant plant, string parentNameList)
+        {
+            if (!string.IsNullOrEmpty(parentNameList))
+            {
+                var plntIdQuery =
+                    (from p in db.Plants
+                     where p.Name == parentNameList
+                     select new { p.PlantID }).Single(); // .Single() is like executeScalar(), Sequence must not contain more than one element, otherwise exception is trown.
+
+                int id = plntIdQuery.PlantID;
+                plant.ParentID = id;
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Plants.Add(plant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ParentID = new SelectList(db.Plants, "PlantID", "Name", plant.ParentID);
+            return View(plant);
+        }
+
+        // GET: Plants/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            //TODO: hier keine clientseitige unique-pruefung
+            return RedirectToAction("UnderConstruction");
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            //Plant plant = db.Plants.Find(id);
+            //if (plant == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            //return View(plant);
+        }
+
+        // POST: Plants/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PlantID,Name,ParentID,HLevelName")] Plant plant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(plant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ParentID = new SelectList(db.Plants, "PlantID", "Name", plant.ParentID);
+            return View(plant);
+        }
+
+        // GET: Plants/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Plant plant = db.Plants.Find(id);
+            if (plant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(plant);
+        }
+
+        // POST: Plants/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Plant plant = db.Plants.Find(id);
+            db.Plants.Remove(plant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Plants/UnderConstruction
+        public ActionResult UnderConstruction()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public JsonResult IsPlantInDB(string Name)
+        {
+            //check if any of the Plant Names in db matches the Name specified in the Parameter using the ANY extension method.  
+            return Json(!db.Plants.Any(x => x.Name == Name), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Plants2/Models/Plant.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace Plants2.Models
+{
+    public class Plant
+    {
+        [Key]
+        public int PlantID { get; set; } 
+        [Required]
+        [StringLength(50)]
+        [Index("IX_Name", 1, IsUnique = true)]
+        [Remote("IsPlantInDB", "Plants", ErrorMessage = "This Plant is already in the Database.")]
+        public string Name { get; set; }
+        public int? ParentID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Level")]
+        public string HLevelName { get; set; }
+        [ForeignKey("ParentID")]
+        public virtual Plant Parent { get; set; }
+        public virtual ICollection<Plant> Childs { get; set; }
+    }
+
+    public class Plant2DBContext : DbContext
+    {
+        public DbSet<Plant> Plants { get; set; }
+    }
+}
